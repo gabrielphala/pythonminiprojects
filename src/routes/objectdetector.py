c39811bfbfab85c418c9642bcb735a101c276412
gabrielphala <gabrielphala@outlook.com>
2023-03-15 13:01:09 +0200

    more error handling

--- src/routes/objectdetector.py
+++ src/routes/objectdetector.py
@@ -22,32 +22,32 @@ def objectDetectorRoute (app):
     
     @app.route('/objectdetector/detect', methods = ['POST'])
     def detectOject ():
-        if ('file' not in request.files) or (request.files['file'] and request.files['file'].filename == ''):
-            return json.dumps({ 'error': 'Please select a file', 'successful': False })
-
-        file = request.files['file']
-
-        if (not(allowed_file(file.filename))):
-            return json.dumps({ 'error': 'File type not allowed', 'successful': False })
-
-        filename = secure_filename(file.filename) 
-        filelocation = os.path.normpath(os.path.dirname(__file__) + '/../static/uploads/' + filename) # get file location
-        
-        file.save(filelocation)
-        
-        resnet = models.resnet101(pretrained=True)
-
-        preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
-        ])
-
         try:
+            if ('file' not in request.files) or (request.files['file'] and request.files['file'].filename == ''):
+                return json.dumps({ 'error': 'Please select a file', 'successful': False })
+
+            file = request.files['file']
+
+            if (not(allowed_file(file.filename))):
+                return json.dumps({ 'error': 'File type not allowed', 'successful': False })
+
+            filename = secure_filename(file.filename) 
+            filelocation = os.path.normpath(os.path.dirname(__file__) + '/../static/uploads/' + filename) # get file location
+            
+            file.save(filelocation)
+            
+            resnet = models.resnet101(pretrained=True)
+
+            preprocess = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406],
+                    std=[0.229, 0.224, 0.225]
+                )
+            ])
+
             img = Image.open(filelocation)
 
             img_t = preprocess(img)
