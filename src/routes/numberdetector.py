64635b46f91235d80439ab087095c4cc1c00d9b2
gabrielphala <gabrielphala@outlook.com>
2023-03-15 13:01:48 +0200

    more error handling

--- src/routes/numberdetector.py
+++ src/routes/numberdetector.py
@@ -23,22 +23,27 @@ def numberDetectorRoute (app):
 
         ext = file.filename.split('.')[-1]
 
-        if not(allowed_file(file.filename, 1)):
+        if not(allowed_file(file.filename)):
             return json.dumps({ 'error': 'File extention now allowed', 'succesful': False }); 
+    
+        try:
+            image_path = os.path.normpath(os.path.dirname(__file__) + '/../static/uploads/' + file.filename)
+            file.save(image_path)
+            
+            image = tf.keras.utils.load_img(image_path,  grayscale=True,  color_mode='grayscale' ,target_size=(28,28))
+            image = tf.keras.utils.img_to_array(image)
 
-        image_path = os.path.normpath(os.path.dirname(__file__) + '/../static/uploads/' + file.filename)
-        file.save(image_path)
-        
-        image = tf.keras.utils.load_img(image_path,  grayscale=True,  color_mode='grayscale' ,target_size=(28,28))
-        image = tf.keras.utils.img_to_array(image)
+            image = image.reshape((1, image.shape[0], image.shape[1],image.shape[2]))
 
-        image = image.reshape((1, image.shape[0], image.shape[1],image.shape[2]))
+            model = tf.keras.models.load_model(os.path.normpath(os.path.dirname(__file__) + '/../static/model/number'))
+            
+            results = model.predict(image)
+            highest = np.argmax(results)
+            
+            results = results.flatten()
 
-        model = tf.keras.models.load_model(os.path.normpath(os.path.dirname(__file__) + '/../static/model/number'))
-        
-        results = model.predict(image)
-        highest = np.argmax(results)
-        
-        results = results.flatten()
+            return json.dumps({ 'successful': True, 'number': int(highest) })
+        except Exception as e:
+            print(e)
 
-        return json.dumps({ 'successful': True, 'number': int(highest) })
+            return json.dumps({ 'error': 'File could not be read', 'successful': False })
